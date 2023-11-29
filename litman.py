31c08f6c45afe0dca45e999c6d36cbf0503a4547
Mike Wallbank <wallbank@fnal.gov>
2023-11-29 13:46:37 -0600

    First fully-working version of LitMan

--- litman.py
+++ litman.py
@@ -20,19 +20,20 @@ class LitMan:
 
         # Define a custom reference format
         journal_strip = config.journal.replace(' ', '').replace('.', '')
-        reference = journal_strip+'_'+str(config.issue)+'_'+config.number+'_'+str(config.year)
+        label = journal_strip+'_'+str(config.issue)+'_'+config.number+'_'+str(config.year)
 
         # Copy file
         copy_path = '{}/{}'.format(self.LitManFiles, config.category.lower())
         if not os.path.exists(copy_path):
             os.mkdir(copy_path)
         extension = os.path.splitext(os.path.basename(config.file))[1]
-        copy_file = '{}/{}'.format(copy_path, reference+extension)
+        copy_file = '{}/{}'.format(copy_path, label+extension)
         shutil.copy(config.file, copy_file)
 
         # Add to database
         new_ref = {
-            reference: {
+            label: {
+                "label": label,
                 "title": config.title,
                 "authors": config.authors,
                 "journal": config.journal,
@@ -43,7 +44,9 @@ class LitMan:
                 "original_file": os.path.abspath(config.file),
                 "tags": [config.category.lower()] + [t.lower() for t in config.tags],
                 "notes": [],
-                "reference": reference
+                "printed": False,
+                "references": [],
+                "citations": []
             }
         }
         with open(self.LitManDB, 'a') as outfile:
@@ -56,9 +59,63 @@ class LitMan:
             with open(self.LitManCache, 'wb') as litman_pickle_file:
                 pickle.dump(litman_db, litman_pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
 
+    def Edit(self, config):
+        # Get the database
+        litman_db = self.LoadDB(config)
+
+        # Find the requested reference
+        if config.ref not in litman_db:
+            raise ValueError("Requested reference {} not in database.".format(config.ref))
+        ref = litman_db[config.ref]
+
+        # Make edits
+        if config.printed is not None and config.printed:
+            ref["printed"] = True
+        if config.add_tag is not None:
+            ref["tags"].append(config.add_tag)
+        if config.rm_tag is not None:
+            ref["tags"].remove(config.rm_tag)
+        if config.rm_note is not None:
+            del ref["notes"][config.rm_note]
+        if config.title is not None:
+            ref["title"] = config.title
+        if config.authors is not None:
+            ref["authors"] = config.authors
+        if config.journal is not None:
+            ref["journal"] = config.journal
+        if config.issue is not None:
+            ref["issue"] = config.issue
+        if config.number is not None:
+            ref["number"] = config.number
+        if config.year is not None:
+            ref["year"] = config.year
+
+        self.Resave(litman_db)
+
+    def Link(self, config):
+        # Get the database
+        litman_db = self.LoadDB(config)
+
+        # Find the requested reference
+        if config.ref not in litman_db:
+            raise ValueError("Requested reference {} not in database.".format(config.ref))
+        ref = litman_db[config.ref]
+
+        # Find the citated reference
+        if config.cite not in litman_db:
+            raise ValueError("Requested reference {} not in database.".format(config.cite))
+        cite = litman_db[config.cite]
+
+        # Link!
+        ref["references"].append(config.cite)
+        cite["citations"].append(config.ref)
+        
+        self.Resave(litman_db)
+
     def List(self, config):
-        entries = self.Winnow(config)
-        self.Print(entries, config)
+        litman_db = self.LoadDB(config)
+        entries = self.Winnow(litman_db, config)
+        self.Print(litman_db, entries, config)
 
     def LoadDB(self, config):
         if config.no_cache:
@@ -70,10 +127,8 @@ class LitMan:
         return litman_db
 
     def Note(self, config):
-        # Get the database
-        litman_db = self.LoadDB(config)
-
         # Find the requested reference
+        litman_db = self.LoadDB(config)
         if config.ref not in litman_db:
             raise ValueError("Requested reference {} not in database.".format(config.ref))
         ref = litman_db[config.ref]
@@ -82,13 +137,12 @@ class LitMan:
         for note in config.note:
             ref['notes'].append(note)
 
-        # Resave
-        with open(self.LitManDB, 'w') as outfile:
-            yaml.dump(litman_db, outfile, default_flow_style=False, allow_unicode=True)
-        self.Cache()
+        self.Resave(litman_db)
 
     def Open(self, config):
-        entries = self.Winnow(config)
+        # Get the requested entries
+        litman_db = self.LoadDB(config)
+        entries = self.Winnow(litman_db, config)
         if not len(entries):
             print("\nNo entries found.\n")
         if config.all:
@@ -97,21 +151,44 @@ class LitMan:
             file_list = entries[0]['file']
         os.system("open {}".format(file_list))
 
-    def Print(self, entries, config):
-        print("\nMatching entries:\n")
+    def Print(self, litman_db, entries, config):
+        
+        print("\033[1;5m\nMatching entries:\033[0m\n")
+        
         for entry in entries:
-            print("{}:\n  {}, {}, {} {} {} ({})"
-                  .format(entry['reference'], entry['title'], entry['authors'], entry['journal'], entry['issue'], entry['number'], entry['year']))
-            if config.notes:
+            print("{}: \033[7m{}\033[0m\n  {}\n    {}\n    {} {} {} ({})"
+                  .format('\033[34m{}\033[30m'.format(entry['label']),
+                          "Printed" if entry["printed"] else "",
+                          '\033[4m{}\033[0m'.format(entry['title']),
+                          '\033[3m{}\033[0m'.format(', '.join(entry['authors'])),
+                          entry['journal'],
+                          '\033[1m{}\033[0m'.format(entry['issue']),
+                          entry['number'],
+                          entry['year']))
+            
+            if not config.compact:
+                print("  (\033[31m{}\033[30m)".format(' '.join(entry['tags'])))
                 for note in entry['notes']:
-                    print("    - {}".format(note))
+                    print("\033[2m    - {}\033[0m".format(note))
+
+            if config.links:
+                print("  \033[32mCitations:\033[0m")
+                for citation in entry['citations']:
+                    cite = litman_db[citation]
+                    print("    - {}: {}".format(cite['label'], cite['title']))
+                print("  \033[32mReferences:\033[0m")
+                for reference in entry['references']:
+                    ref = litman_db[reference]
+                    print("    - {}: {}".format(ref['label'], ref['title']))
+                
             print()
 
-    def Winnow(self, config):
+    def Resave(self, litman_db):
+        with open(self.LitManDB, 'w') as outfile:
+            yaml.dump(litman_db, outfile, default_flow_style=False, allow_unicode=True)
+        self.Cache()
 
-        # Get the database
-        litman_db = self.LoadDB(config)
-
+    def Winnow(self, litman_db, config):
         # Return all entries by default
         entries = list(litman_db.values())
 
@@ -120,7 +197,7 @@ class LitMan:
             entries = []
             for ref in config.ref:
                 if ref not in litman_db:
-                    raise ValueError("Requested entry {} not in database.".format(config.reference))
+                    raise ValueError("Requested entry {} not in database.".format(config.label))
                 entries.append(litman_db[ref])
 
         # Search
@@ -152,11 +229,13 @@ def ParseArguments():
 
     parser = argparse.ArgumentParser(prog="litman",
                                      description="Literature Manager: Managing literature for the lit man.")
+    parser.add_argument("--no_cache", action='store_true',
+                        help="Force load from the YAML file rather than the cache.")
     subparser = parser.add_subparsers(title="litman command", dest="command")
     subparser.required = True
 
     # Cache
-    cache_parser = subparser.add_parser("cache", help="Remake the LitMan cache.")
+    cache_parser = subparser.add_parser("cache", help="Rebuild the LitMan cache.")
 
     # Add
     add_parser = subparser.add_parser("add", help="Add reference to LitMan.")
@@ -179,6 +258,38 @@ def ParseArguments():
     add_parser.add_argument("--tags", type=str, nargs='+', required=True,
                             help="Tags for organizing and retrieving the reference.")
 
+    # Edit
+    edit_parser = subparser.add_parser("edit", help="Edit existing reference.")
+    edit_parser.add_argument("--ref", type=str, required=True,
+                             help="LitMan reference.")
+    edit_parser.add_argument("--printed", action="store_true",
+                             help="Add note to the reference that it has been printed.")
+    edit_parser.add_argument("--add_tag", type=str,
+                             help="Add tag to reference.")
+    edit_parser.add_argument("--rm_tag", type=str,
+                             help="Remove tag from reference.")
+    edit_parser.add_argument("--rm_note", type=int,
+                             help="Remove note (by note index, starting from 0) from reference.")
+    edit_parser.add_argument("--title", type=str,
+                             help="Title.")
+    edit_parser.add_argument("--authors", type=str, nargs='+',
+                             help="Authors.")
+    edit_parser.add_argument("--journal", type=str,
+                             help="Journal.")
+    edit_parser.add_argument("--issue", type=int,
+                             help="Journal issue.")
+    edit_parser.add_argument("--number", type=str,
+                             help="Article number.")
+    edit_parser.add_argument("--year", type=int,
+                             help="Publication year.")
+
+    # Link
+    link_parser = subparser.add_parser("link", help="Link references.")
+    link_parser.add_argument("--ref", type=str, required=True,
+                             help="Reference in LitMan to add link to.")
+    link_parser.add_argument("--cite", type=str, required=True,
+                             help="Literature cited by this reference.")
+
     # List
     list_parser = subparser.add_parser("list", help="List references in LitMan.")
     list_parser.add_argument("--search", type=str, nargs='+',
@@ -187,12 +298,10 @@ def ParseArguments():
                              help="LitMan reference.")
     list_parser.add_argument("--authors", type=str, nargs='+',
                              help="Authors to filter.")
-    list_parser.add_argument("--notes", action='store_true',
-                             help="Print notes for each item.")
+    list_parser.add_argument("--compact", action='store_true',
+                             help="Print items in a compact view.")
     list_parser.add_argument("--links", action='store_true',
                              help="Print links for each item.")
-    list_parser.add_argument("--no_cache", action='store_true',
-                             help="Force load from the YAML file rather than the cache.")
 
     # Open
     open_parser = subparser.add_parser("open", help="Open file for reference in LitMan.")
@@ -202,8 +311,6 @@ def ParseArguments():
                              help="LitMan reference.")
     open_parser.add_argument("--all", action='store_true',
                              help="Open all matching entries.")
-    open_parser.add_argument("--no_cache", action='store_true',
-                             help="Force load from the YAML file rather than the cache.")
 
     # Note
     note_parser = subparser.add_parser("note", help="Add note to existing reference.")
@@ -211,8 +318,6 @@ def ParseArguments():
                              help="LitMan reference.")
     note_parser.add_argument("--note", type=str, nargs='+', required=True,
                              help="Add note to reference; multiple entries can be made in bullet-point style.")
-    note_parser.add_argument("--no_cache", action='store_true',
-                             help="Force load from the YAML file rather than the cache.")
 
     return parser.parse_args()
 
@@ -234,6 +339,10 @@ def main():
             litman.Cache()
         case "add":
             litman.Add(config)
+        case "edit":
+            litman.Edit(config)
+        case "link":
+            litman.Link(config)
         case "list":
             litman.List(config)
         case "open":
