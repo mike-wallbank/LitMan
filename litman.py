8e7d89b614e23b86dcf617eac42d711f3530dae0
Mike Wallbank <wallbank@fnal.gov>
2024-01-02 13:40:04 -0600

    Add backup functionality; simplify initial set up and configuration of LitMan

--- litman.py
+++ litman.py
@@ -43,7 +43,7 @@ class LitManReference:
         self.citations = []
 
     def Strip(self, attribute):
-        return attribute.replace(' ', '').replace('.', '').replace(':', '')
+        return attribute.replace(' ', '').replace('.', '').replace(':', '').replace('(', '').replace(')', '')
 
 # Article reference type
 class LitManArticle(LitManReference):
@@ -175,15 +175,27 @@ reference_types = {"article":LitManArticle(),
                    "book":LitManBook()}
 
 class LitMan:
-    def __init__(self, directory):
-        self.LitManDir = directory
-        self.LitManDB = directory+'/litman.yaml'
+    def __init__(self, config):
+
+        # Set required paths
+        if 'directory' not in config or 'backup' not in config:
+            raise RuntimeError("\nLitMan directory and backup required (use \033[1mlitman setup\033[0m).\n")
+        self.LitManDir = config['directory']
+        if not os.path.exists(self.LitManDir):
+            raise RuntimeError("\nLitMan directory {} does not exist.".format(self.LitManDir))
+        self.BackupDir = config['backup']
+        if not os.path.exists(self.LitManDir):
+            raise RuntimeError("\nLitMan directory {} does not exist.".format(self.LitManDir))
+
+        # Set database files
+        self.LitManDB = self.LitManDir+'/litman.yaml'
         if not os.path.exists(self.LitManDB):
-            os.system('touch {}'.format(self.LitManDB))
-        self.LitManFiles = directory+'/files/'
+            with open(self.LitManDB, 'w'):
+                os.utime(self.LitManDB, None)
+        self.LitManFiles = self.LitManDir+'/files/'
         if not os.path.exists(self.LitManFiles):
             os.mkdir(self.LitManFiles)
-        self.LitManCache = directory+'/litman.pickle'
+        self.LitManCache = self.LitManDir+'/litman.pickle'
 
     def Add(self, config):
 
@@ -193,7 +205,7 @@ class LitMan:
 
         # Ensure this is a new entry
         litman_db = self.LoadDB(config)
-        if new_ref.label in litman_db:
+        if litman_db is not None and new_ref.label in litman_db:
             raise NameError("Reference {} already exists in database.".format(new_ref.label))
 
         # Copy file
@@ -216,22 +228,22 @@ class LitMan:
             yaml.dump(dict({new_ref.label: vars(new_ref)}), outfile, default_flow_style=False, allow_unicode=True)
         self.Cache()
 
+    def Backup(self):
+        with open("{}/.litman".format(os.path.expanduser("~")), 'r') as litman_config_file:
+            litman_config = yaml.safe_load(litman_config_file)
+        if 'backup' not in litman_config:
+            raise RuntimeError("\nLitMan backup directory not set (use \033[1mlitman setup --backup\033[0m to add).\n")
+        backup_directory = litman_config['backup']
+        if not os.path.exists(backup_directory):
+            raise RuntimeError("LitMan backup directory {} does not exist.".format(backup_directory))
+        copy_path = backup_directory+'/'+os.path.basename(self.LitManDB)+'.backup'
+        shutil.copy(self.LitManDB, copy_path)
+
     def Cache(self):
         with open(self.LitManDB, 'r') as litman_db_file:
             litman_db = yaml.safe_load(litman_db_file)
             with open(self.LitManCache, 'wb') as litman_pickle_file:
                 pickle.dump(litman_db, litman_pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
-
-    def Confirm(self, prompt):
-        choice = None
-        while choice is None:
-            in_choice = input(prompt+" [y/n] ")
-            if in_choice.lower() == 'y' or in_choice.lower() == 'yes':
-                choice = 'y'
-            if in_choice.lower() == 'n' or in_choice.lower() == 'no':
-                choice = 'n'
-        if choice == 'n':
-            exit()
 
     def Edit(self, config):
         # Get the database
@@ -248,24 +260,24 @@ class LitMan:
             ref["tags"].sort()
 
         if config.rm_tag is not None:
-            self.Confirm("Remove tag {} from {}?".format(config.rm_tag, config.ref))
+            Confirm("Remove tag {} from {}?".format(config.rm_tag, config.ref))
             ref["tags"].remove(config.rm_tag)
 
         if config.rm_note is not None:
-            self.Confirm("Remove note {} from {}?".format(config.rm_note, config.ref))
+            Confirm("Remove note {} from {}?".format(config.rm_note, config.ref))
             del ref["notes"][config.rm_note]
 
         if config.rm_ref is not None:
             cited = litman_db[ref["references"][config.rm_ref]]
-            self.Confirm("Remove reference {} from {} (and citation {} from {})?"
-                         .format(cited['label'], config.ref, config.ref, cited['label']))
+            Confirm("Remove reference {} from {} (and citation {} from {})?"
+                    .format(cited['label'], config.ref, config.ref, cited['label']))
             ref["references"].remove(cited['label'])
             cited["citations"].remove(config.ref)
 
         if config.rm_cite is not None:
             refed = litman_db[ref["citations"][config.rm_cite]]
-            self.Confirm("Remove citation {} from {} (and reference {} from {})?"
-                         .format(refed['label'], config.ref, config.ref, refed['label']))
+            Confirm("Remove citation {} from {} (and reference {} from {})?"
+                    .format(refed['label'], config.ref, config.ref, refed['label']))
             ref['citations'].remove(refed['label'])
             refed['references'].remove(config.ref)
 
@@ -299,7 +311,7 @@ class LitMan:
         self.PrintReferences(litman_db, entries, config)
 
     def LoadDB(self, config):
-        if config.no_cache:
+        if config.no_cache or not os.path.exists(self.LitManCache):
             with open(self.LitManDB, 'r') as litman_db_file:
                 litman_db = yaml.safe_load(litman_db_file)
         else:
@@ -388,15 +400,17 @@ class LitMan:
                 print("  \033[32mCitations:\033[0m")
                 for citation in entry['citations']:
                     cite = litman_db[citation]
-                    print("    - {}: {}".format(cite['label'], cite['title']))
+                    print("    - {}: {} ({})".format(cite['label'], cite['title'], cite['year']))
                 print("  \033[32mReferences:\033[0m")
                 for reference in entry['references']:
                     ref = litman_db[reference]
-                    print("    - {}: {}".format(ref['label'], ref['title']))
+                    print("    - {}: {} ({})".format(ref['label'], ref['title'], ref['year']))
 
             print()
 
     def Remove(self, config):
+        Confirm("Remove reference {}?".format(config.ref))
+
         # Load the database
         litman_db = self.LoadDB(config)
 
@@ -415,9 +429,15 @@ class LitMan:
         self.Resave(litman_db)
 
     def Resave(self, litman_db):
-        with open(self.LitManDB, 'w') as outfile:
-            yaml.dump(litman_db, outfile, default_flow_style=False, allow_unicode=True)
+        if len(litman_db):
+            with open(self.LitManDB, 'w') as outfile:
+                yaml.dump(litman_db, outfile, default_flow_style=False, allow_unicode=True)
+        else:
+            os.remove(self.LitManDB)
+            with open(self.LitManDB, 'w'):
+                os.utime(self.LitManDB, None)            
         self.Cache()
+        self.Backup()
 
     def Summary(self, config):
         # Load the database
@@ -446,6 +466,9 @@ class LitMan:
         print()
 
     def Winnow(self, litman_db, config):
+        if litman_db is None:
+            return list()
+        
         # Return all entries by default
         entries = list(litman_db.values())
 
@@ -502,8 +525,21 @@ class LitMan:
 
         return entries
 
+def Confirm(prompt):
+    choice = None
+    while choice is None:
+        in_choice = input("\n"+prompt+" [y/n] ")
+        if in_choice.lower() == 'y' or in_choice.lower() == 'yes':
+            choice = 'y'
+        if in_choice.lower() == 'n' or in_choice.lower() == 'no':
+            choice = 'n'
+        print()
+        if choice == 'n':
+            exit()
+
 def ParseArguments():
 
+    # Set up parser
     parser = argparse.ArgumentParser(prog="litman",
                                      description="Literature Manager: Managing literature for the lit man.")
     parser.add_argument("--no_cache", action='store_true',
@@ -511,8 +547,23 @@ def ParseArguments():
     subparser = parser.add_subparsers(title="litman command", dest="command")
     subparser.required = True
 
+    # Setup
+    setup_parser = subparser.add_parser("setup", help="Set up LitMan.")
+    setup_parser.add_argument("--litman_dir", required=True,
+                              help="Base directory for LitMan to use for its organization.")
+    setup_parser.add_argument("--backup_dir", required=True,
+                              help="Backup directory for LitMan to store a copy of the database.")
+
+    # Config
+    config_parser = subparser.add_parser("config", help="Configuration options.")
+    config_parser.add_argument("--litman_dir", help="Base directory for LitMan to use for its organization.")
+    config_parser.add_argument("--backup_dir", help="Backup directory for LitMan to store a copy of the database.")
+
     # Cache
     cache_parser = subparser.add_parser("cache", help="Rebuild the LitMan cache.")
+
+    # Backup
+    backup_parser = subparser.add_parser("backup", help="Backup the LitMan database.")
 
     # Add
     add_parser = subparser.add_parser("add", help="Add reference to LitMan.")
@@ -624,22 +675,58 @@ def ParseArguments():
 
     return parser.parse_args()
 
+def SetupLitManEnv(config):
+
+    # Ensure a configuration file
+    litman_config_file = "{}/.litman".format(os.path.expanduser("~"))
+    if not os.path.exists(litman_config_file):
+        print("\nLitMan configuration file not found; making new file {}... ".format(litman_config_file), end='')
+        with open(litman_config_file, 'a'):
+            os.utime(litman_config_file, None)
+        print("\033[32mDone.\033[0m\n")
+    else:
+        Confirm("Overwrite existing configuration file {}?".format(litman_config_file))
+
+    # Save configuration
+    configuration = {'directory':os.path.abspath(config.litman_dir),
+                     'backup':os.path.abspath(config.backup_dir)}
+    with open(litman_config_file, 'w') as config_file:
+        yaml.dump(configuration, config_file)
+
 def main():
 
     # Parse arguments
     config = ParseArguments()
 
-    # Set up database
+    # Set up
+    if config.command == 'setup':
+        SetupLitManEnv(config)
+        exit()
+
+    # Check configuration file
+    litman_config_file = "{}/.litman".format(os.path.expanduser("~"))
+    if not os.path.exists(litman_config_file):
+        print("\nLitMan configuration file not found; use \033[1mlitman setup\033[0m to configure LitMan.\n")
+        exit()
+
+    # Read litman configuration file
     with open("{}/.litman".format(os.path.expanduser("~")), 'r') as litman_config_file:
         litman_config = yaml.safe_load(litman_config_file)
-    if 'directory' not in litman_config:
-        raise RunTimeError("LitMan directory required.")
-    litman = LitMan(litman_config['directory'].replace('~', os.path.expanduser("~")))
+    if litman_config is None:
+        raise RuntimeError("\nLitMan configuration file is empty; use \033[1mlitman setup\033[0m to configure.\n")
+
+    # Set up program
+    litman = LitMan(litman_config)
 
     # Do stuff
     match config.command:
+        case "config":
+            print("Config not yet implemented.")
+            exit()
         case "cache":
             litman.Cache()
+        case "backup":
+            litman.Backup()
         case "add":
             litman.Add(config)
         case "edit":
